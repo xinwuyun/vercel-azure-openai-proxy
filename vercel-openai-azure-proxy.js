0fe69d022a4669a918e2a03948d6a7ba2a4a10ce
Zerui Wang <zrwangxdu@outlook.com>
2023-10-26 02:19:19 +0800

    Update vercel-openai-azure-proxy.js

--- vercel-openai-azure-proxy.js
+++ vercel-openai-azure-proxy.js
@@ -1,49 +1,59 @@
-// The name of your Azure OpenAI Resource for GPT-3.5 and GPT-4.
-const resourceName = "fubaov1";
+// The name of your Azure OpenAI Resource.
+const resourceName=process.env.RESOURCE_NAME
 
-// The deployment name you chose when you deployed the model for GPT-3.5 and GPT-4.
-const deployNameGPT35 = process.env.DEPLOY_NAME_GPT35;
-const deployNameGPT35_16K = process.env.DEPLOY_NAME_GPT35_16K;
-const deployNameGPT4 = process.env.DEPLOY_NAME_GPT4;
+// The deployment name you chose when you deployed the model.
 const mapper = {
-    'gpt-3.5-turbo': "gpt3",
-    'gpt-3.5-turbo-16k': "gpt-35-turbo-16k",
-    // 'gpt-4': DEPLOY_NAME_GPT4,
-    // 'gpt-4-32k': DEPLOY_NAME_GPT4_32K,
+    'gpt-3.5-turbo': process.env.DEPLOY_NAME_GPT35,
+    'gpt-3.5-turbo-16k': process.env.DEPLOY_NAME_GPT35_16K,
+    // 'gpt-4': process.env.DEPLOY_NAME_GPT4,
+    // 'gpt-4-32k': process.env.DEPLOY_NAME_GPT4_32K,
 };
+
 const apiVersion="2023-08-01-preview"
 
-async function handleRequest(request,res, path) {
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
   if (request.method === 'OPTIONS') {
-    return handleOPTIONS(request, res)
+    return handleOPTIONS(request)
   }
 
-  // const url = new URL(request.url);
-  const url = new URL(path, 'http://localhost'); // Use a dummy base URL
+  const url = new URL(request.url);
+  if (url.pathname.startsWith("//")) {
+    url.pathname = url.pathname.replace('/',"")
+  }
   if (url.pathname === '/v1/chat/completions') {
     var path="chat/completions"
   } else if (url.pathname === '/v1/completions') {
     var path="completions"
   } else if (url.pathname === '/v1/models') {
-    return handleModels(request, res)
+    return handleModels(request)
   } else {
-    res.status(404).send('404 Not Found');
-    return;
+    return new Response('404 Not Found', { status: 404 })
   }
-  
+
   let body;
   if (request.method === 'POST') {
-    body = request.body;
+    body = await request.json();
   }
-  const modelName = body && body.model ? body.model : "gpt-3.5-turbo";
-  const { resourceName, deployName } = getModelMapper(modelName);
 
+  const modelName = body?.model;  
+  const deployName = mapper[modelName] || '' 
+
+  if (deployName === '') {
+    return new Response('Missing model mapper', {
+        status: 403
+    });
+  }
   const fetchAPI = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`
-  
-  const authKey = request.get('Authorization');
+
+  const authKey = request.headers.get('Authorization');
   if (!authKey) {
-    res.status(403).send('Not allowed');
-    return;
+    return new Response("Not allowed", {
+      status: 403
+    });
   }
 
   const payload = {
@@ -55,34 +65,36 @@ async function handleRequest(request,res, path) {
     body: typeof body === 'object' ? JSON.stringify(body) : '{}',
   };
 
-  // let { readable, writable } = new TransformStream()
-  const response = await fetch(fetchAPI, payload);
-  if (response.status !== 200) {
-    res.status(response.status).send(response.statusText);
-    return;
-  }
-  res.setHeader('Content-Type', response.headers.get('Content-Type'));
-  await stream(response.body, res);
+  let response = await fetch(fetchAPI, payload);
+  response = new Response(response.body, response);
+  response.headers.set("Access-Control-Allow-Origin", "*");
+
+  if (body?.stream != true){
+    return response
+  } 
+
+  let { readable, writable } = new TransformStream()
+  stream(response.body, writable);
+  return new Response(readable, response);
+
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function getModelMapper(model) {
-  const deployName = mapper[model] || '';
-  if (deployName === '') {
-    throw new Error("Invalid model specified");
-  }
-  return { resourceName: resourceName, deployName:  deployName};
-}
-
-async function stream(readable, res) {
+// support printer mode and add newline
+async function stream(readable, writable) {
   const reader = readable.getReader();
+  const writer = writable.getWriter();
+
+  // const decoder = new TextDecoder();
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
-  const delimiter = "\n\n";
-  const encodedNewline = encoder.encode("\n");
+// let decodedValue = decoder.decode(value);
+  const newline = "\n";
+  const delimiter = "\n\n"
+  const encodedNewline = encoder.encode(newline);
 
   let buffer = "";
   while (true) {
@@ -95,24 +107,29 @@ async function stream(readable, res) {
 
     // Loop through all but the last line, which may be incomplete.
     for (let i = 0; i < lines.length - 1; i++) {
-      res.write(lines[i] + delimiter);
-      await sleep(30);
+      await writer.write(encoder.encode(lines[i] + delimiter));
+      await sleep(20);
     }
+
     buffer = lines[lines.length - 1];
   }
 
   if (buffer) {
-    res.write(buffer);
+    await writer.write(encoder.encode(buffer));
   }
-  res.write(encodedNewline);
-  res.end();
+  await writer.write(encodedNewline)
+  await writer.close();
 }
 
-async function handleModels(request, res) {
+async function handleModels(request) {
   const data = {
     "object": "list",
-    "data": [ {
-      "id": "gpt-3.5-turbo",
+    "data": []  
+  };
+
+  for (let key in mapper) {
+    data.data.push({
+      "id": key,
       "object": "model",
       "created": 1677610602,
       "owned_by": "openai",
@@ -130,21 +147,23 @@ async function handleModels(request, res) {
         "group": null,
         "is_blocking": false
       }],
-      "root": "gpt-3.5-turbo",
+      "root": key,
       "parent": null
-    }]
-  };
-  res.status(200).json(data);
+    });  
+  }
+
+  const json = JSON.stringify(data, null, 2);
+  return new Response(json, {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
 
-async function handleOPTIONS(request, res) {
-    res.set({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers': '*'
-    }).status(200).send();
+async function handleOPTIONS(request) {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    })
 }
-
-module.exports = {
-  handleRequest
-};
